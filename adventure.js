55640f40760558e1f3546898ac1a0077879a9858
adelavega <136393880+meadowsoup@users.noreply.github.com>
2025-02-10 15:45:50 -0500

    understanding changes

--- adventure.js
+++ adventure.js
@@ -10,13 +10,28 @@ const adventurer = {
                name: "bane",
                type: "dragon",
                inventory: ["bowler hat", "sunglasses"]
-          },
-          roll (mod = 0) {
-               const result = Math.floor(Math.random() * 20) + 1 + mod;
-               console.log(`${this.name} rolled a ${result}.`)
           }
+     },
+     roll (mod = 0) {
+          const result = Math.floor(Math.random() * 20) + 1 + mod;
+          console.log(`${this.name} rolled a ${result}.`)
      }
-}
+
+};
+// checking if inventory works
+console.log("Sol's Inventory:");
+adventurer.inventory.forEach(item => console.log(item));
+
+// access nested "companions inventory"
+console.log("Sunny's companion Bane has:");
+adventurer.companion.companion.inventory.forEach(item => console.log(item));
+
+// dice roll testing
+console.log("Dice Roll");
+adventurer.roll();
+adventurer.roll(2);
+adventurer.roll(5);
+adventurer.roll(10);
 
 class Character {
      constructor(name) {
@@ -24,6 +39,11 @@ class Character {
           this.health = 100;
           this.inventory = [];
      }
+     roll (mod = 0) {
+          const result = Math.floor(Math.random() * 20) + 1 + mod;
+          console.log(`${this.name} rolled a ${result}.`)
+     }
+
 }
 
 const sol = new Character("Sol");
@@ -34,17 +54,25 @@ sol.companion.companion = new Character("bane");
 sol.companion.companion.type = "dragon";
 sol.companion.companion.inventory = ["bowler hat", "sunglasses"];
 
+class Character {
+     constructor(name) {
+          this.name = name;
+          this.health = Character.MAX_HEALTH;
+          this.inventory = [];
+     }
+     
+     static MAX_HEALTH = 100;
+}
+
 class Adventurer extends Character {
+     static ROLES = ["Rune Master", "Spirit Master", "Black Smith"];
+     
      constructor(name, role) {
           super(name);
-          // adventurers have specialized roles.
-          this.role = role;
-          // every adventurer starts with a bed and 50 gold.
-          this.inventory.push("bedroll", "50 gold coins");
-     }
-     // Adventurers have the ability to scout ahead of them.
-     scout() {
-          console.log(`${this.name} is scouting ahead...`);
-          super.roll();
+          if (!Adventurer.ROLES.includes(role)) {
+               throw new Error(`${role} is not valid! Choose from: ${Adventurer.ROLES.join(", ")}`)
+          }
+
+          this.role = role
      }
 }
