792e72fd1e5fc916db7c9f1409ade0323dc826dd
adelavega <136393880+meadowsoup@users.noreply.github.com>
2025-02-10 13:13:44 -0500

    adventure start

---
+++ adventure.js
@@ -0,0 +1,19 @@
+//* Adventurer is the OBJECT! from which we can access things (inventory especially)
+const adventurer = {
+     name: "Sol",
+     health: 25,
+     inventory: ["sword", "potion", "artifact"],
+     companion: {        // object within object....
+          name: "sunny",
+          type: "slime",
+          companion: {
+               name: "bane",
+               type: "dragon",
+               inventory: ["bowler hat", "sunglasses"]
+          },
+          roll (mod = 0) {
+               const result = Math.floor(Math.random() * 20) + 1 + mod;
+               console.log(`${this.name} rolled a ${result}.`)
+          }
+     }
+}
